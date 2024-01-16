0601c2726120a7f18ad1303ff932a384888d6cb4
Gwanghun-Im <eames@imgwanghun-ui-MacBookPro.local>
2024-01-16 14:13:39 +0900

    fst Commit

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontSize: '28',
+        color:'#999999'}}
+      >
+        Hello! I made a RN App!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: '28',
+    color:'#999999'
+  }
 });

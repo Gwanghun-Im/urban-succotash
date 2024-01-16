6094cb1adc7a5ea8b2d7fb3b8cfc212960180b13
Gwanghun-Im <eames@imgwanghun-ui-MacBookPro.local>
2024-01-16 16:21:22 +0900

    weather app 2.5

--- App.js
+++ App.js
@@ -1,38 +1,21 @@
 // OS의 상단 바
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{
-        fontSize: 28,
-        color:'#999999'}}
-      >
-        Hello! I made a RN App!
-      </Text>
-      <StatusBar style="inverted" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex:1, backgroundColor: 'tomato'}}></View>
+        <View style={{ flex:2, backgroundColor: 'teal'}}></View>
+        <View style={{ flex:1, backgroundColor: 'orange'}}></View>
+      </View>
+      <View style={{ flex: 1, flexDirection:'row' }}>
+        <View style={{ flex:1, backgroundColor: 'tomato'}}></View>
+      </View>
     </View>
   );
 }
-// StyleSheet.create로 autocomplete를 사용할 수 있다.
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#191919',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-    color:'#999999'
-  }
-});
-const styles2 = {
-  container: {
-    flex: 1,
-    backgroundColor: '#191919',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-};
+
+// 레이아웃에서 width와 height를 숫자로 설정하지 않는다
+// 대신, flex를 사용한다.

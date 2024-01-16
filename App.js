16bc838cd052b9c808af9c711fb59b774183e9fb
Gwanghun-Im <eames@imgwanghun-ui-MacBookPro.local>
2024-01-16 16:10:14 +0900

    weather app 2.4

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// OS의 상단 바
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,16 +6,16 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{
-        fontSize: '28',
+        fontSize: 28,
         color:'#999999'}}
       >
         Hello! I made a RN App!
       </Text>
-      <StatusBar style="auto" />
+      <StatusBar style="inverted" />
     </View>
   );
 }
-
+// StyleSheet.create로 autocomplete를 사용할 수 있다.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,7 +24,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontSize: '28',
+    fontSize: 28,
     color:'#999999'
   }
 });
+const styles2 = {
+  container: {
+    flex: 1,
+    backgroundColor: '#191919',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+};
